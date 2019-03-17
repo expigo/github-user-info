c184c6f52ac5111910be9bb34c03f6e5e4e8ebf4
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-03-17 21:42:25 +0100

    Prepare to deploy

--- src/middleware.js
+++ src/middleware.js
@@ -1,10 +1,14 @@
 import { json, urlencoded } from 'body-parser'
 import cors from 'cors'
 import morgan from 'morgan'
+import compression from 'compression'
+import helmet from 'helmet'
 
 export const setGlobalMiddleware = app => {
   app.use(cors())
   app.use(json())
   app.use(urlencoded({ extended: true }))
-  app.use(morgan('dev'))
+  app.use(compression())
+  app.use(helmet())
+  app.use(morgan('combined'))
 }
